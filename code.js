03c8fd39203cc187f9d00b754e152cb4341137d5
zknapt <knappts1@gmail.com>
2018-11-06 21:15:29 +0000

    revise text based on feedback and add new iframe

--- code.js
+++ code.js
@@ -2,3 +2,4 @@
 // Get new ideas off the ground
 
 //https://www.mapsmadeeasy.com/maps/public_3D/4d4d909a162343979289039a76ad1ed0
+//https://www.mapsmadeeasy.com/maps/public/8461da85c242470595e0981eed9e67bd
