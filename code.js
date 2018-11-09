1f125bbbdfef3d601999c4d78e35391f4dbcdfea
zknapt <knappts1@gmail.com>
2018-11-09 16:16:52 +0000

    added navbar, placeholder images and revised color palette

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-// Make drones your secret weapon
 // Get new ideas off the ground
+//Certain conditions can make flying impractical, but we always work hard to be flexible in the face of adverse weather.
 
 //https://www.mapsmadeeasy.com/maps/public_3D/4d4d909a162343979289039a76ad1ed0
-// https://www.mapsmadeeasy.com/maps/public_3D/8461da85c242470595e0981eed9e67bd
+//https://www.mapsmadeeasy.com/maps/public_3D/8461da85c242470595e0981eed9e67bd
