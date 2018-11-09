3094adc7c27408ba8f458b76a662e702f593dece
zknapt <knappts1@gmail.com>
2018-11-09 16:43:54 +0000

    delayed loading of iframe by 4 seconds

--- code.js
+++ code.js
@@ -3,3 +3,7 @@
 
 //https://www.mapsmadeeasy.com/maps/public_3D/4d4d909a162343979289039a76ad1ed0
 //https://www.mapsmadeeasy.com/maps/public_3D/8461da85c242470595e0981eed9e67bd
+
+setTimeout(function(){
+	document.getElementById('martello').src = "https://www.mapsmadeeasy.com/maps/public_3D/8461da85c242470595e0981eed9e67bd";
+}, 4000);
